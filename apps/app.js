94c4108e434cd93b2098bc2dd4f039d5bd3a186a
Emiliakazoo <emiliakazoo@gmail.com>
2016-10-15 07:04:30 -0700

    getting titles

--- apps/app.js
+++ apps/app.js
@@ -1,7 +1,10 @@
 $(function(){
 
-  $.getJSON('https://www.omdbapi.com/?s=Star%20Wars&r=json', function(){
-    console.log(data);
+  $.getJSON('https://www.omdbapi.com/?s=Star%20Wars&r=json', function(data){
+  	var movieData = data.Search;
+  	$.each(movieData, function(index, value){
+  		console.log(value.Title);
+  	});
   });
 
 });
